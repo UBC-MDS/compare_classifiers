81cc51646018303000852e74b9c2bda29f879c47
Susannah Sun <thisis.susannahsun@gmail.com>
2025-01-14 01:08:59 -0800

    completed ensemble_predict + unit tests; tests all pass; added additional test data; will handle warnings later

--- src/compare_classifiers/ensemble_predict.py
+++ src/compare_classifiers/ensemble_predict.py
@@ -1,5 +1,12 @@
+import pandas as pd, numpy as np
+
+import sklearn
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import StackingClassifier
+from sklearn.base import is_classifier
+
+ESTIMATOR_ERROR = 'first parameter has to be a list of (name, estimator) tuples where name is a string and estimator is a sklearn Classifier or pipeline'
+METHOD_ERROR = 'fourth parameter has to be a string of two possible values: "voting" and "stacking"'
 
 def ensemble_predict(estimators, X_train, y_train, ensemble_method, test_data):
     """predict class for test data with provided estimators and whether predicting through Voting or Stacking
@@ -9,8 +16,8 @@ def ensemble_predict(estimators, X_train, y_train, ensemble_method, test_data):
     estimators : list of tuples
         Individual estimators to be processed through the voting or stacking classifying ensemble. Each tuple contains a string: label of estimator, and a model: the estimator.
 
-    X_train : Pandas data frame
-        Data frame containing training data along with n features.
+    X_train : Pandas data frame or Numpy array
+        Data frame containing training data along with n features or ndarray with no feature names.
         
     y_train : Pandas series
         Target class labels for data in X_train.
@@ -33,11 +40,62 @@ def ensemble_predict(estimators, X_train, y_train, ensemble_method, test_data):
     ...     ('svm', make_pipeline(StandardScaler(), LinearSVC(random_state=42)))
     ... ]
     >>> ensemble_predict(estimators, X, y, unseen_data, 'voting')
-    """    
+    """
+
+    # Check if estimators is a list
+    if not isinstance(estimators, list):
+        raise TypeError(ESTIMATOR_ERROR)
+    # Iterate through each element in the list
+    for item in estimators:
+        # Check if the item is a tuple with exactly two elements
+        if not (isinstance(item, tuple) and len(item) == 2):
+            raise TypeError(ESTIMATOR_ERROR)
+        # Check if the first element is a string
+        if not isinstance(item[0], str):
+            raise TypeError(ESTIMATOR_ERROR)
+        # Check if the second element is an instance of an sklearn classifier
+        if not (is_classifier(item[1]) or isinstance(item[1], sklearn.pipeline.Pipeline)):
+            raise TypeError(ESTIMATOR_ERROR)
+    
+    # Check if X_train is Pandas data frame or Numpy array
+    if not (isinstance(X_train, pd.DataFrame) or isinstance(X_train, np.ndarray)):
+        raise TypeError('second parameter has to be a Pandas data frame or Numpy array containing training data')   
+    # Check if X_train contains data
+    empty_df = isinstance(X_train, pd.DataFrame) and X_train.empty
+    empty_ndarr = isinstance(X_train, np.ndarray) and X_train.size == 0
+    if (empty_df or empty_ndarr):
+        raise ValueError('second parameter seems to be an empty Pandas data frame or Numpy array. Please ensure data is present.') 
+    
+    # Check if y_train is Pandas series
+    if not isinstance(y_train, pd.Series):
+        raise TypeError('third parameter has to be a Pandas series')
+    # Check if y_train contains data
+    if y_train.empty:
+        raise ValueError('third parameter seems to be an empty Pandas series. Please ensure your series contains data.') 
+    
+    # Check if ensemble_method is string
+    if not isinstance(ensemble_method, str):
+        raise TypeError(METHOD_ERROR)
+    # Check if ensemble_method is either 'voting' or 'stacking'
+    if (not ensemble_method == 'voting' and not ensemble_method == 'stacking'):
+        raise ValueError(METHOD_ERROR)
+    
+    # Check if test_data is a Pandas data frame
+    if not (isinstance(test_data, pd.DataFrame) or isinstance(test_data, np.ndarray)):
+        raise TypeError('fifth parameter has to be a Pandas data frame or Numpy array containing training data')
+    # Check if test_data contains data
+    empty_df = isinstance(test_data, pd.DataFrame) and test_data.empty
+    empty_ndarr = isinstance(test_data, np.ndarray) and test_data.size == 0
+    if (empty_df or empty_ndarr):
+        raise ValueError('fifth parameter seems to be an empty Pandas data frame or Numpy array. Please ensure data is present.') 
+
+    # Return predictions if voting    
     if ensemble_method == 'voting':
         ev = VotingClassifier(estimators)
         ev = ev.fit(X_train, y_train)
         return ev.predict(test_data)
+    
+    # Return predictions if stacking
     if ensemble_method == 'stacking':
         sc = StackingClassifier(estimators)
         sc = sc.fit(X_train, y_train)
--- tests/test_ensemble_predict.py
+++ tests/test_ensemble_predict.py
@@ -5,10 +5,15 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from tests.test_data import test_data, models
 
+import pytest
+
+import pandas as pd
 import numpy as np
 
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import StackingClassifier
+
+# Create test data
 
 data_dict = test_data()
 X_train = data_dict['X_train']
@@ -42,64 +47,162 @@ multi_pipe = [
     ('pipe_mnp', model_dict['pipe_mnp'])
 ]
 
-"""Raises error when estimators is not a list of sklearn Classifiers."""
-"""Raises error when X_train is not a pandas data frame."""
-"""Raises error when y_train is not a pandas series."""
-"""Raises error when ensemble_method is not a string of either 'voting' or 'stacking'."""
-"""Raises error when test_data is not a pandas data frame."""
+VOTING = 'voting'
+STACKING = 'stacking'
+
+
+# Error handling test cases
+
+def estimators_incorrect_type():
+    """Raises error when estimators is not a list of (name, estimator) tuples where name is a string and estimator is a sklearn Classifier or pipeline."""
+    with pytest.raises(TypeError):
+        ensemble_predict('', X_train, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(1, X_train, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(True, X_train, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict({}, X_train, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict((), X_train, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict([('', )], X_train, y_train, VOTING, X_test_ss)
+
+def test_X_train_incorrect_type():
+    """Raises error when X_train is not a pandas data frame."""
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, '', y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, 1, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, True, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, {}, y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, (), y_train, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, y_train, y_train, VOTING, X_test_ss)
+
+def test_X_train_empty_dataframe():
+    """Raises error when X_train is empty"""
+    with pytest.raises(ValueError):
+        ensemble_predict(knn5_and_mnb, pd.DataFrame({"A": []}), y_train, VOTING, X_test_ss)
+
+def test_y_train_incorrect_type():
+    """Raises error when y_train is not a pandas series."""
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, '', VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, 1, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, True, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, {}, VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, (), VOTING, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, X_train, VOTING, X_test_ss)
+
+def test_y_train_empty_series():
+    """Raises error when y_train is empty"""
+    with pytest.raises(ValueError):
+        ensemble_predict(knn5_and_mnb, X_train, pd.Series([]), VOTING, X_test_ss)
+
+def test_ensemble_method_incorrect_type():
+    """Raises error when ensemble_method is not a string of either 'voting' or 'stacking'."""
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, 1, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, True, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, {}, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, (), X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, X_train, X_test_ss)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, y_train, X_test_ss)
+
+def test_ensemble_predict_incorrect_value():
+    """Raises error when ensemble_method is not string of neither 'voting' nor 'stacking'."""
+    with pytest.raises(ValueError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, '', X_test_ss)
+
+def test_test_data_incorrect_type():
+    """Raises error when test_data is not a pandas data frame."""
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, '')
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, 1)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, True)
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, {})
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, ())
+    with pytest.raises(TypeError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, y_train)
+
+def test_test_data_empty_dataframe():
+    """Raises error when test_data is empty"""
+    with pytest.raises(ValueError):
+        ensemble_predict(knn5_and_mnb, X_train, y_train, VOTING, pd.DataFrame({"A": []}))
+
+
+# Success test cases
 
 def test_individual_voting_success():
     """When estimators is a list of individual Classifiers, returns the same numpy array as sklearn's VotingClassifier when ensemble_method is 'voting'."""
     vc = VotingClassifier(knn5_and_mnb)
     vc = vc.fit(X_train_ss, y_train)
     predictions = vc.predict(X_test_ss)
-    assert(np.all(ensemble_predict(knn5_and_mnb, X_train_ss, y_train, 'voting', X_test_ss) == predictions))
+    assert(np.all(ensemble_predict(knn5_and_mnb, X_train_ss, y_train, VOTING, X_test_ss) == predictions))
 
 def test_individual_stacking_success():
     """When estimators is a list of individual Classifiers, returns the same numpy array as sklearn's StackingClassifier when ensemble_method is 'stacking'."""
     sc = StackingClassifier(knn5_and_mnb)
     sc = sc.fit(X_train_ss, y_train)
     predictions = sc.predict(X_test_ss)
-    assert(np.all(ensemble_predict(knn5_and_mnb, X_train_ss, y_train, 'stacking', X_test_ss) == predictions))
+    assert(np.all(ensemble_predict(knn5_and_mnb, X_train_ss, y_train, STACKING, X_test_ss) == predictions))
 
 def test_pipeline_voting_success():
     """When estimators is a list of pipelines, returns the same numpy array as sklearn's VotingClassifier when ensemble_method is 'voting'."""
     vc = VotingClassifier(two_pipes)
     vc = vc.fit(X_train, y_train)
     predictions = vc.predict(X_test_rs)
-    assert(np.all(ensemble_predict(two_pipes, X_train, y_train, 'voting', X_test_rs) == predictions))
+    assert(np.all(ensemble_predict(two_pipes, X_train, y_train, VOTING, X_test_rs) == predictions))
 
 def test_pipeline_stacking_success():
     """When estimators is a list of pipelines, returns the same numpy array as sklearn's StackingClassifier when ensemble_method is 'stacking'."""
     sc = StackingClassifier(two_pipes)
     sc = sc.fit(X_train, y_train)
     predictions = sc.predict(X_test_rs)
-    assert(np.all(ensemble_predict(two_pipes, X_train, y_train, 'stacking', X_test_rs) == predictions))
+    assert(np.all(ensemble_predict(two_pipes, X_train, y_train, STACKING, X_test_rs) == predictions))
 
 def test_multi_individual_voting_success():
     """When estimators is a list of more than 2 individual Classifiers, returns the same numpy array as sklearn's VotingClassifier when ensemble_method is 'voting'."""
     vc = VotingClassifier(multi_ind)
     vc = vc.fit(X_train_ss, y_train)
     predictions = vc.predict(X_test_ss)
-    assert(np.all(ensemble_predict(multi_ind, X_train_ss, y_train, 'voting', X_test_ss) == predictions))
+    assert(np.all(ensemble_predict(multi_ind, X_train_ss, y_train, VOTING, X_test_ss) == predictions))
 
 def test_multi_individual_stacking_success():
     """When estimators is a list of more than 2 individual Classifiers, returns the same numpy array as sklearn's StackingClassifier when ensemble_method is 'stacking'."""
     sc = StackingClassifier(multi_ind)
     sc = sc.fit(X_train_ss, y_train)
     predictions = sc.predict(X_test_ss)
-    assert(np.all(ensemble_predict(multi_ind, X_train_ss, y_train, 'stacking', X_test_ss) == predictions))
+    assert(np.all(ensemble_predict(multi_ind, X_train_ss, y_train, STACKING, X_test_ss) == predictions))
 
 def test_multi_pipeline_voting_success():
     """When estimators is a list of more than 2 pipelines, returns the same numpy array as sklearn's VotingClassifier when ensemble_method is 'voting'."""
     vc = VotingClassifier(multi_pipe)
     vc = vc.fit(X_train, y_train)
     predictions = vc.predict(X_test_rs)
-    assert(np.all(ensemble_predict(multi_pipe, X_train, y_train, 'voting', X_test_rs) == predictions))
+    assert(np.all(ensemble_predict(multi_pipe, X_train, y_train, VOTING, X_test_rs) == predictions))
 
 def test_multi_pipeline_stacking_success():
     """When estimators is a list of more than 2 pipelines, returns the same numpy array as sklearn's StackingClassifier when ensemble_method is 'stacking'."""
     sc = StackingClassifier(multi_pipe)
     sc = sc.fit(X_train, y_train)
     predictions = sc.predict(X_test_rs)
-    assert(np.all(ensemble_predict(multi_pipe, X_train, y_train, 'stacking', X_test_rs) == predictions))
+    assert(np.all(ensemble_predict(multi_pipe, X_train, y_train, STACKING, X_test_rs) == predictions))
